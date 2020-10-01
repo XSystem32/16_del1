4a60f80f8a9841c66208db5dba8420c92ef72c23
unknown <x_system16@hotmail.com>
2020-10-01 19:50:35 +0200

    Har tilføjet castdice metode

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    //Denne metode returnerer et tilfældig nummmer fra 1 til 6
+    public static int castDice(){
+        int min = 1;
+        int max = 6;
+        int range = max - min + 1;
+
+        return (int)(Math.random() * range) + min;
+    }
+
 }
