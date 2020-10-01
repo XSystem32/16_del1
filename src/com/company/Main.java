b8e39cf3a88173a603deba7b85ca174e5394c511
thomas <thomas.arildtoft@live.dk>
2020-10-01 20:00:14 +0200

    Boolean Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,33 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+
+        boolean end = true;
+
+        //Vi laver en while loop, fordi vi vil gerne kaster terningerne indtil en af spillerne vinder
+        //Det betyder her, at while loop kommer til at køre indtil boolen end bliver til false
+        while (end) {
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
     }
+
+
+
 
     //Denne metode returnerer et tilfældig nummmer fra 1 til 6
     public static int castDice(){
